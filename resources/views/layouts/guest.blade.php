e763d38fc876cb530a61750301fdd58182d57a26
MiladFattah <miladalpha1379@gmail.com>
2022-02-20 20:19:44 +0330

    start Home page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,7 @@
         <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v30.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/swiper-bundle.css') }}">
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
@@ -19,7 +20,44 @@
     <body class="bg-[#f9fafb]">
         <div class="min-h-screen font-Vazir text-gray-900 antialiased dark:bg-gray-700">
         <x-navbar></x-navbar>
-            {{ $slot }}
+           <div class="container mx-auto">
+             {{ $slot }}
+           </div>
         </div>
     </body>
+
+
+    <script src="{{asset('js/swiper-bundle.min.js')}}"></script>
+
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          // when window width is >= 768
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+          },
+          1248: {
+            slidesPerView: 4,
+            spaceBetween: 40
+          },
+        }
+      });
+    </script>
 </html>
